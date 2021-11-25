1344d79bdcc9058e1d96e8c64f9ab206e6a78838
Alan Wang <alan098555@gmail.com>
2021-11-25 22:27:03 +1300

    Home Page End 2 End Test With Cypress

--- cypress/integration/end2end.spec.js
+++ cypress/integration/end2end.spec.js
@@ -20,6 +20,10 @@ describe( 'User visit the website', () =>
         //Video Text and Button Loaded
         cy.findByRole( 'heading', { name: /enjoy summer holidays/i } ).should( 'exist' )
         cy.findByRole( 'link', { name: /explore/i } ).should( 'exist' )
+        cy.findByRole( 'link', { name: /explore/i } ).click()
+        cy.location( 'pathname' ).should( 'include', 'guides' )
+        cy.wait( 1000 )
+        cy.go( -1 )
 
         //Explore Nearby Loaded
         cy.findByRole( 'img', { name: /wellington/i } ).should( 'exist' )
@@ -27,9 +31,23 @@ describe( 'User visit the website', () =>
         //Discover Banner Text and Button Loaded
         cy.findByRole( 'heading', { name: /discover valuable tourings/i } ).should( 'exist' )
         cy.findByRole( 'link', { name: /discover/i } ).should( 'exist' )
+        cy.findByRole( 'link', { name: /discover/i } ).click()
+        cy.location( 'pathname' ).should( 'include', 'touring' )
+        cy.wait( 1000 )
+        cy.go( -1 )
 
         //Footer Loaded
         cy.get( 'footer' ).should( 'exist' )
         cy.findByRole( 'link', { name: /about us/i } ).should( 'exist' )
+        cy.findByRole( 'link', { name: /about us/i } ).click()
+        cy.location( 'pathname' ).should( 'include', 'about' )
+        cy.wait( 1000 )
+        cy.go( -1 )
+
+        cy.get( 'footer' ).within( () =>
+        {
+            cy.get( 'svg' ).should( 'exist' )
+            cy.get( 'svg' ).first().parent().should( 'have.attr', 'href', 'https://twitter.com' )
+        } )
     } )
 } )
